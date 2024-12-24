98bd967bff8ddda7ab5a1313874d94fcbebaf93a
AnhDung31 <vadanhdungh@gmail.com>
2024-12-24 19:01:11 +0700

    update some file

--- client/src/redux/action/spinner.js
+++
@@ -1,9 +0,0 @@
-import { TURN_OFF, TURN_ON } from "../constant/spinner";
-
-export const turnOnLoadingAction = () => ({
-  type: TURN_ON,
-});
-
-export const turnOffLoadingAction = () => ({
-  type: TURN_OFF,
-});
--- client/src/utils/getAccessToken.js
+++
@@ -1,6 +0,0 @@
-export const getAccessToken = () => {
-  const user = localStorage.getItem("USER");
-  if (!user) return ""
-   return JSON.parse(user)?.accessToken;
-};
-//console.log('user local',getAccessToken())
