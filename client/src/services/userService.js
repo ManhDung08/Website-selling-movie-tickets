21d640415d3a9097adaee2fa76f3b5001e894ca2
vandungthieu <vandungthieu20102003@gmail.com>
2024-12-24 23:36:36 +0700

    cap nhat app

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -18,12 +18,3 @@ export const login = async (loginData) => {
       throw error;
     }
   };
-  export const userDetail = async (id) => {
-    try {
-      const response = await axiosInstance.get(`users/${id}`);
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-      throw error;
-    }
-  };
